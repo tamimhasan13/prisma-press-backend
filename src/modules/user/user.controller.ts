741ec3d55a67342cca6b3887bda98492221b9451
tamimhasan13 <tamimhasan975@gmail.com>
2026-06-24 15:43:40 +0600

    async as higer order functin and utils added

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,33 +1,59 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, RequestHandler, Response } from "express";
 import { prisma } from "../../lib/prisma";
 import bcrypt from "bcryptjs";
 import config from "../../config";
 import httpStatus from "http-status";
 import { userService } from "./user.service";
+import { catchAsync } from "../../utills/catchAsync";
+import { sendResponse } from "../../utills/sendResponse";
 
-const registerUser = async (req: Request, res: Response) => {
-  try {
+
+
+// const registerUser = async (req: Request, res: Response) => {
+//   try {
+//     const payload = req.body;
+//     const user = await userService.registerUserIntoDB(payload);
+//     res.status(httpStatus.CREATED).json({
+//       success: true,
+//       statusCode: httpStatus.CREATED,
+//       message: "User registered successfully",
+//       data: {
+//         user,
+//       },
+//     });
+//   } catch (error) {
+//      console.log(error);
+
+//      res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+//        success: false,
+//        statusCode: httpStatus.INTERNAL_SERVER_ERROR,
+//        message: "Failed to register user",
+//        error: (error as Error).message,
+//      });
+//   }
+// };
+const registerUser =catchAsync(async (req: Request, res: Response,next:NextFunction)=> {
+  
     const payload = req.body;
     const user = await userService.registerUserIntoDB(payload);
-    res.status(httpStatus.CREATED).json({
-      success: true,
-      statusCode: httpStatus.CREATED,
-      message: "User registered successfully",
-      data: {
-        user,
-      },
+    // res.status(httpStatus.CREATED).json({
+    //   success: true,
+    //   statusCode: httpStatus.CREATED,
+    //   message: "User registered successfully",
+    //   data: {
+    //     user,
+    //   },
+    // });
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.CREATED,
+        message: "User registered successfully",
+        data: {
+          user,
+        },
     });
-  } catch (error) {
-     console.log(error);
-
-     res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-       success: false,
-       statusCode: httpStatus.INTERNAL_SERVER_ERROR,
-       message: "Failed to register user",
-       error: (error as Error).message,
-     });
-  }
-};
+  
+});
 export const userController={
     registerUser
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -21,14 +21,19 @@ const registerUserIntoDB=async(payload:RegisterUserPayload)=>{
         name,
         email,
         password: hashedPassword,
-      },
+        profile:{
+            create:{
+                profilePhoto
+            }
+        }
+      }
     });
-    await prisma.profile.create({
-      data: {
-        userId: createdUser.id,
-        profilePhoto,
-      },
-    });
+    // await prisma.profile.create({
+    //   data: {
+    //     userId: createdUser.id,
+    //     profilePhoto,
+    //   },
+    // });
     const user = await prisma.user.findUnique({
       where: {
         id: createdUser.id,
